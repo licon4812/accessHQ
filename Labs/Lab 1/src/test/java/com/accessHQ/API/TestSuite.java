d5ff4014eb32c43fcfd7f76377477445e9c19526
James Alickolli <jalickolli@gmail.com>
2022-06-28 13:05:04 +1000

    Completed StatusCode 200 test

--- Labs/Lab 1/src/test/java/com/accessHQ/API/TestSuite.java
+++ Labs/Lab 1/src/test/java/com/accessHQ/API/TestSuite.java
@@ -1,2 +1,19 @@
-package com.accessHQ.API;public class TestSuite {
+package com.accessHQ.API;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class TestSuite {
+    @Test
+    public void Api_StatusCode_is200(){
+        given().
+                header("auth-key", "1ce06293-733b-4776-8fcb-0250b712a0c9").
+                param("q", "Tascott").
+                param("state", "NSW").
+        when()
+                .get("https://digitalapi.auspost.com.au/postcode/search.json").
+                then().
+                assertThat().statusCode(200);
+    }
 }
